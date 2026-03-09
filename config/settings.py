40d35cb32114e5180f1e1216273e8351602a38ac
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-09 18:22:03 +0500

    nmagap

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ew0v=_la=f6pkd_xq$@1_(960)^3hx%90cr8m%-401+*wi(((9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Simpleapp.pythonanywhere.com']
 
 
 # Application definition
